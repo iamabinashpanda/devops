ac0933cefc3858de081af5d283dc340b3729add8
Abinash <iamabinashpanda@outlook.com>
2025-04-07 16:07:46 +0530

    Test NG and ALlure Demo test for Selenium

---
+++ src/test/java/test/BasicValidation.java
@@ -0,0 +1,55 @@
+package test;
+
+import static org.testng.Assert.assertTrue;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+
+public class BasicValidation {
+
+	public static WebDriver driver;
+	String appUrl = "https://www.facebook.com";
+
+	@BeforeTest
+	public void setup() {
+		driver = new ChromeDriver();
+		driver.get(appUrl);
+	}
+
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
+
+	@Test
+	public void ValidateUrl() {
+		String expectedResult = "https://www.facebook.com/";
+		String actualResult = driver.getCurrentUrl();
+
+		Reporter.log("Expected Result = " + expectedResult);
+		Reporter.log("Actual Result = " + actualResult);
+
+		assertTrue(actualResult.equals(expectedResult), "Mismatch in application URL");
+
+	}
+
+	@Test
+	public void welcomeMessageValidation() {
+		String expectedResult = "Facebook Helps you";
+		String actualResult = driver.findElement(By.xpath("//h2[@class='_8eso']")).getText();
+
+		Reporter.log("Expected Result = " + expectedResult);
+		Reporter.log("Actual Result = " + actualResult);
+
+		assertTrue(actualResult.equals(expectedResult), "Mismatch in welcome message");
+
+	}
+
+}
